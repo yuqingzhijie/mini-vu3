841216d90f218217da49d58c759bb9f5b44e2c44
lisa <2711676102@qq.com>
2023-02-14 23:18:00 +0800

    stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,36 @@
-
+import { extend } from "../shared";
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
+  public scheduler: Function | undefined;
 
-  constructor(fn, public scheduler?) {
+  constructor(fn, scheduler?: Function) {
     this._fn = fn;
+    this.scheduler = scheduler
   }
 
   run () {
     activeEffect = this;
     return this._fn();
   }
+
+  stop () {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect (effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  })
 }
 
 const targetMap = new Map()
@@ -26,7 +47,10 @@ export function track (target, key) {
     depsMap.set(key, dep);
   }
 
+  if(!activeEffect) return
+
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger (target, key) {
@@ -44,10 +68,21 @@ export function trigger (target, key) {
 
 let activeEffect;
 export function effect(fn, options: any = {}) {
+  // fn
   const scheduler = options.scheduler;
+  // scheduler
   const _effect = new ReactiveEffect(fn, scheduler);
+  // options
+  extend(_effect, options);
 
   _effect.run()
 
-  return _effect.run.bind(_effect);
+  const runner: any =  _effect.run.bind(_effect);
+  runner.effect = _effect;
+
+  return runner;
+}
+
+export function stop (runner) {
+  runner.effect.stop();
 }
