2f13f659b7b2f79e2eef81b8fbe5c20e8fed3017
lisa <2711676102@qq.com>
2023-02-18 11:49:25 +0800

    reactive和readonly嵌套逻辑实现

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,5 +1,6 @@
+import { isObject } from "../shared";
 import { track, trigger } from "./effect";
-import { ReactiveFlags } from "./reactive";
+import { reactive, ReactiveFlags, readonly } from "./reactive";
 
 const get = createGetter();
 const set = createSetter();
@@ -15,6 +16,10 @@ export function createGetter(isReadonly = false) {
     }
 
     const res = Reflect.get(target, key);
+
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res);
+    }
   
     if (!isReadonly) {
       track(target, key);
