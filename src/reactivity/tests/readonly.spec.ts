dd78c55c03a59e3b7d9185c8f90a8e6df7a400f6
lisa <2711676102@qq.com>
2023-02-18 12:11:31 +0800

    isProxy

--- src/reactivity/tests/readonly.spec.ts
+++ src/reactivity/tests/readonly.spec.ts
@@ -1,4 +1,4 @@
-import { isReadonly, readonly } from "../reactive";
+import { isProxy, isReadonly, readonly } from "../reactive";
 
 describe('readonly', () => {
   it('happy path', () => {
@@ -7,7 +7,9 @@ describe('readonly', () => {
     const wrapped = readonly(original);
     expect(wrapped).not.toBe(original);
     expect(isReadonly(wrapped)).toBe(true);
+    expect(isReadonly(original)).toBe(false);
     expect(isReadonly(wrapped.bar)).toBe(true);
+    expect(isProxy(wrapped)).toBe(true);
     expect(wrapped.foo).toBe(1);
   });
 
