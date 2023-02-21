c61aff8b29311893c2980ec5ca6d4224c87ea4ae
lisa <2711676102@qq.com>
2023-02-21 21:42:15 +0800

    isRef, unRef, proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -6,6 +6,7 @@ class RefImpl {
   private _value: any;
   private _rawValue: any;
   public dep;
+  public __v_isRef = true;
 
   constructor (value) {
     this._rawValue = value;
@@ -41,4 +42,28 @@ function trackRefValue(ref) {
 
 export function ref (value) {
   return new RefImpl(value);
+}
+
+export function isRef (ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef (ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs (objWithRefs) {
+  return new Proxy(objWithRefs, {
+    get (target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+
+    set (target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return(target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    }
+  })
 }
