cd03d27985c7d186fa5853047fafdd4e8238a372
yuqingzhijie <2711676102@qq.com>
2023-03-24 22:13:07 +0800

    解析template 插值， element标签

---
+++ src/compiler-core/src/parse.ts
@@ -0,0 +1,90 @@
+import { NodeTypes } from './ast';
+
+const enum TagType {
+  Start,
+  End,
+}
+
+export function baseParse(content: string) {
+  const context = createParserContext(content);
+
+  return createRoot(parseChildren(context));
+}
+
+function parseChildren(context) {
+  const nodes: any = [];
+
+  let node;
+  const s = context.source;
+  if (s.startsWith('{{')) {
+    node = parseInterpolation(context);
+  } else if (s[0] === '<') {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
+  }
+  nodes.push(node);
+  return nodes;
+}
+
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+
+  return element;
+}
+
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+
+  if (type === TagType.End) {
+    return;
+  }
+
+  return {
+    type: NodeTypes.ELEMENT,
+    tag: 'div',
+  };
+}
+
+function parseInterpolation(context) {
+  const openDelimiter = '{{';
+  const closeDelimiter = '}}';
+
+  const closeIndex = context.source.indexOf(closeDelimiter, openDelimiter.length);
+
+  advanceBy(context, openDelimiter.length);
+
+  const rawContentLength = closeIndex - openDelimiter.length;
+  const rawContent = context.source.slice(0, rawContentLength);
+  const content = rawContent.trim();
+
+  advanceBy(context, rawContentLength + closeDelimiter.length);
+
+  return {
+    type: NodeTypes.INTERPOLATION,
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content: content,
+    },
+  };
+}
+
+function advanceBy(context: any, length: number) {
+  context.source = context.source.slice(length);
+}
+
+function createRoot(children) {
+  return {
+    children,
+  };
+}
+
+function createParserContext(content: string) {
+  return {
+    source: content,
+  };
+}
---
+++ src/compiler-core/tests/parse.spec.ts
@@ -0,0 +1,31 @@
+import { NodeTypes } from '../src/ast';
+import { baseParse } from '../src/parse';
+
+describe('Parse', () => {
+  describe('interpolation', () => {
+    test('simple interpolation', () => {
+      const ast = baseParse('{{ message }}');
+
+      // root
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.INTERPOLATION,
+        content: {
+          type: NodeTypes.SIMPLE_EXPRESSION,
+          content: 'message',
+        },
+      });
+    });
+  });
+
+  describe('element', () => {
+    test('simple element div', () => {
+      const ast = baseParse('<div></div>');
+
+      // root
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.ELEMENT,
+        tag: 'div',
+      });
+    });
+  });
+});
