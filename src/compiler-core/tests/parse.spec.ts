8bb077901938335ccc05c0fd58a90c58191b4fa3
yuqingzhijie <2711676102@qq.com>
2023-03-25 13:06:03 +0800

    解析template text

--- src/compiler-core/tests/parse.spec.ts
+++ src/compiler-core/tests/parse.spec.ts
@@ -28,4 +28,16 @@ describe('Parse', () => {
       });
     });
   });
+
+  describe('text', () => {
+    test('simple text', () => {
+      const ast = baseParse('some text');
+
+      // root
+      expect(ast.children[0]).toStrictEqual({
+        type: NodeTypes.TEXT,
+        content: 'some text',
+      });
+    });
+  });
 });
