5ea1e6b3087266c91f565cd95a1220bb231a3c1d
yuqingzhijie <2711676102@qq.com>
2023-04-02 16:42:19 +0800

    generate生成代码string

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,8 +1,14 @@
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   // 1、遍历 - 深度优先搜索
   traverseNode(root, context);
   // 2、修改 text content
+
+  createRootCodegen(root);
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
 }
 
 function createTransformContext(root: any, options: any) {
@@ -25,6 +31,7 @@ function traverseNode(node: any, context) {
 
   traverseChildren(node, context);
 }
+
 function traverseChildren(node: any, context: any) {
   const children = node.children;
 
