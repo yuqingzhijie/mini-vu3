158936f2d1e86e2aabac996e51668b0841b5ffbe
yuqingzhijie <2711676102@qq.com>
2023-04-05 16:48:28 +0800

    实现三种类型代码生成

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -6,6 +6,8 @@ export const isObject = (val) => {
   return val !== null && typeof val === 'object';
 };
 
+export const isString = (value) => typeof value === 'string';
+
 export const hasChanged = (val, newVal) => {
   return !Object.is(val, newVal);
 };
